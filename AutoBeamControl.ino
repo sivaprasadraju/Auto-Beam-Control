17fa903dcf6b9a96a90e5934f83d378410ef7559
sraju728@gmail.com <sraju728@gmail.com>
2018-11-11 21:55:39 +0530

    Adding required packages and libraries

---
+++ AutoBeamControl.ino
@@ -0,0 +1,13 @@
+/*
+    Name: Siva Prasad Raju Bairaju
+    Version: version 1.0
+    Case Study Title: Auto Beam Control
+    TinkerCad Link: https://www.tinkercad.com/things/cg5mpkx06k6-tremendous-stantia/editel?tenant=circuits?sharecode=-9RhfYRxUiegCPeckrvqv20HFRHTNZ6USNs2srlcYqQ=
+
+*/
+
+#include<stdint.h>
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include<util/delay.h>
+#include<stdbool.h>
