132bebda59612c6dede5ce72245fb085af6b8440
sraju728@gmail.com <sraju728@gmail.com>
2018-11-11 22:02:55 +0530

    Adding main function and initializing the interrupts

--- AutoBeamControl.ino
+++ AutoBeamControl.ino
@@ -23,3 +23,41 @@ struct
 }FLAG;
 
 volatile int value;
+
+int main()
+{
+  SET_BIT(DDRB,PB1);
+  SET_BIT(DDRD,PD4);
+  
+  CLR_BIT(DDRB,PB1);
+  CLR_BIT(DDRD,PD4);
+
+  CLR_BIT(PORTD,PD3);
+  CLR_BIT(PORTD,PD2);
+  CLR_BIT(DDRC,PC0);
+  
+  EICRA|=(1<<ISC00);
+  EICRA &= ~(1<<ISC01);                                                                                                                                  
+  EIMSK |= (1<<INT0);
+  
+  EICRA|=(1<<ISC10);
+  EICRA &= ~(1<<ISC11);                                                                                                                             
+  EIMSK |= (1<<INT1);
+  
+    
+  TCCR2A = 0x00;
+  TCNT2 = 0x00;
+  TCCR2B|=((1<<CS20)|(1<<CS21)| (1<<CS22));
+  TIMSK2 |= (1<<OCIE2A) | (1<<OCIE2B);
+  
+  Serial.begin(9600);
+  
+  sei();
+  
+  while(1)
+  {
+    //Serial.println("Entered into main loop");
+    SET_BIT(PORTD,PD4);
+    idle_state();
+  }
+}
